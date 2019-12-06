037e775c550ff304c5e0e6db9b0e980d9de49cc3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-06 15:19:39 +0100

    parses the ex4 html file

---
+++ examples/ex4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	link "github.com/Oyekunle-Mark/bug-free-octo-doodle"
+)
+
+var exampleHTML = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">
+    A link to another page
+    <span> some span  </span>
+  </a>
+  <a href="/page-two">A link to a second page</a>
+</body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHTML)
+
+	links, err := link.Parse(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", links)
+}
